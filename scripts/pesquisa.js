7cf420efd6d31cfa513f2533780374026119977f
caslumes <caslumes@outlook.com>
2024-10-23 21:22:19 -0300

    limpando o codigo

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -1,10 +1,7 @@
 window.onload = function(){
     document.querySelectorAll('.filtro').forEach(filtro => {
         filtro.addEventListener('click', function(){
-            console.log('Clique detectado!'); // Verifica se o clique é detectado
-            
             let submenu = this.querySelector('.submenu-filtro');
-            console.log(submenu); // Verifica se o submenu está sendo corretamente selecionado
             submenu.classList.toggle('active');
         });
     });
