1ce38203c9feb39c71eadbf216b463b012afdfb2
caslumes <caslumes@outlook.com>
2024-11-19 01:54:43 -0300

    prototipo carrinho

--- carrinho.php
+++ carrinho.php
@@ -16,6 +16,50 @@
     <header id="header-principal">
     </header>
 
-    
+    <main>
+        <div class="container-produtos">
+            <ul>
+                <li class="item-compra">
+                    <a class="container-imagem-produto" href="">
+                        <img class=imagem-produto src="./imagens/imgstenis/imagemDunk1.png" alt="">
+                    </a>
+                    <div class="container-desc-item">
+                        <div class="container-info-produto">
+                            <p>Nome produto</p>
+                            <p>Tamanho</p>
+                        </div>
+                        <div class="container-qtd-produto">
+                            <a href=""><img class="icone" src="./icones/remover.png" alt=""></a>
+                            
+                            <p>1</p>
+                            <a href=""><img class="icone" src="./icones/adicionar.png" alt=""></a>
+                        </div>
+                        <p class="subtotal">R$1.800,00</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="container-informacoes">
+            <div class="container-calculo">
+                <ul>
+                    <li class="item-calculo">
+                        <p>Produtos</p>
+                        <p>1.800,00</p>
+                    </li>
+                </ul>
+                <div class="total">
+                    <p>Total</p>
+                    <div class="valor">
+                        <h2>R$1800,00 à vista</h2>
+                        <p>ou em até 12x de R$150,00</p>
+                    </div>
+                </div>
+                <a href=""><p>Continuar</p></a>
+            </div>
+        </div>
+    </main>
+
+    <footer id=footer-principal>
+    </footer>
 </body>
 </html>
